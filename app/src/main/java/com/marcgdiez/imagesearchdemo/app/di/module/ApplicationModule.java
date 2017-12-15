991b916d3b4ab1548f363813cf9cff61841e23c4
Marc González Díez <marcgdiez@gmail.com>
2017-12-15 21:10:37 +0100

    Inject Executor class and Main thread at application level

--- app/src/main/java/com/marcgdiez/imagesearchdemo/app/di/module/ApplicationModule.java
+++ app/src/main/java/com/marcgdiez/imagesearchdemo/app/di/module/ApplicationModule.java
@@ -1,7 +1,19 @@
 package com.marcgdiez.imagesearchdemo.app.di.module;
 
+import com.marcgdiez.imagesearchdemo.core.executor.MainThread;
+import com.marcgdiez.imagesearchdemo.core.executor.ThreadExecutor;
 import dagger.Module;
+import dagger.Provides;
+import java.util.concurrent.Executor;
+import javax.inject.Singleton;
 
 @Module public class ApplicationModule {
 
+  @Provides @Singleton public Executor provideExecutor() {
+    return new ThreadExecutor();
+  }
+
+  @Provides @Singleton public MainThread providesMainThread() {
+    return new MainThread();
+  }
 }
