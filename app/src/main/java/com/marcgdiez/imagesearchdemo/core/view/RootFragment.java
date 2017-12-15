b2d03d004432f952acad0e0524414806ac41a035
Marc González Díez <marcgdiez@gmail.com>
2017-12-15 20:45:46 +0100

    Manage Presenter from RootFragment

--- app/src/main/java/com/marcgdiez/imagesearchdemo/core/view/RootFragment.java
+++ app/src/main/java/com/marcgdiez/imagesearchdemo/core/view/RootFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.marcgdiez.imagesearchdemo.core.presenter.Presenter;
 
 public abstract class RootFragment extends Fragment {
 
@@ -26,14 +27,37 @@ public abstract class RootFragment extends Fragment {
     return inflater.inflate(fragmentLayoutResourceId, container, false);
   }
 
+  protected abstract void initializePresenter();
+
   @LayoutRes protected abstract int getFragmentLayoutResourceId();
 
   @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
     initializeView(view);
+    initializePresenter();
   }
 
   protected abstract void initializeView(View view);
 
   protected abstract void initializeInjector();
+
+  @Override public void onResume() {
+    super.onResume();
+
+    Presenter presenter = getPresenter();
+    if (presenter != null) {
+      presenter.start();
+    }
+  }
+
+  @Override public void onStop() {
+    super.onStop();
+
+    Presenter presenter = getPresenter();
+    if (presenter != null) {
+      presenter.stop();
+    }
+  }
+
+  protected abstract Presenter getPresenter();
 }
