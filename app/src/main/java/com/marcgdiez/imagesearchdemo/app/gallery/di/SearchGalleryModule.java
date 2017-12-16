8762b9947a47623c5253ac5f5cd5304534458b0d
Marc González Díez <marcgdiez@gmail.com>
2017-12-16 22:29:28 +0100

    SearchImagesUseCase Injection

--- app/src/main/java/com/marcgdiez/imagesearchdemo/app/gallery/di/SearchGalleryModule.java
+++ app/src/main/java/com/marcgdiez/imagesearchdemo/app/gallery/di/SearchGalleryModule.java
@@ -1,7 +1,17 @@
 package com.marcgdiez.imagesearchdemo.app.gallery.di;
 
+import com.marcgdiez.imagesearchdemo.app.gallery.usecase.SearchImagesUseCase;
+import com.marcgdiez.imagesearchdemo.core.di.PerFragment;
+import com.marcgdiez.imagesearchdemo.core.interactor.Interactor;
+import com.marcgdiez.imagesearchdemo.entity.ImageEntity;
 import dagger.Module;
+import dagger.Provides;
+import java.util.List;
 
 @Module public class SearchGalleryModule {
 
+  @Provides @PerFragment public Interactor<List<ImageEntity>> provideSearchImagesUseCase(
+      SearchImagesUseCase searchImagesUseCase) {
+    return searchImagesUseCase;
+  }
 }
