f52270a83ed441fb4eb8c4d6572e2661f611548a
Marc González Díez <marcgdiez@gmail.com>
2017-12-16 01:03:22 +0100

    Create Repository for Images & Injection

--- app/src/main/java/com/marcgdiez/imagesearchdemo/app/di/module/ApplicationModule.java
+++ app/src/main/java/com/marcgdiez/imagesearchdemo/app/di/module/ApplicationModule.java
@@ -2,6 +2,12 @@ package com.marcgdiez.imagesearchdemo.app.di.module;
 
 import com.marcgdiez.imagesearchdemo.core.executor.MainThread;
 import com.marcgdiez.imagesearchdemo.core.executor.ThreadExecutor;
+import com.marcgdiez.imagesearchdemo.data.ServiceFactory;
+import com.marcgdiez.imagesearchdemo.data.flickr.FlickrApi;
+import com.marcgdiez.imagesearchdemo.data.flickr.FlickrApiConstants;
+import com.marcgdiez.imagesearchdemo.data.flickr.FlickrNetworkDataSource;
+import com.marcgdiez.imagesearchdemo.data.repository.ImagesRepository;
+import com.marcgdiez.imagesearchdemo.data.repository.ImagesRepositoryImpl;
 import dagger.Module;
 import dagger.Provides;
 import java.util.concurrent.Executor;
@@ -15,5 +21,14 @@ import javax.inject.Singleton;
 
   @Provides @Singleton public MainThread providesMainThread() {
     return new MainThread();
+  }
+
+  @Provides @Singleton
+  public ImagesRepository provideImagesRepository(FlickrNetworkDataSource flickrDataSource) {
+    return new ImagesRepositoryImpl(flickrDataSource);
+  }
+
+  @Provides @Singleton public FlickrApi provideFlickrApi() {
+    return ServiceFactory.createRetrofitService(FlickrApi.class, FlickrApiConstants.BASE_URL);
   }
 }
---
+++ app/src/main/java/com/marcgdiez/imagesearchdemo/data/NetworkDataSource.java
@@ -0,0 +1,10 @@
+package com.marcgdiez.imagesearchdemo.data;
+
+import com.marcgdiez.imagesearchdemo.entity.ImageEntity;
+import java.util.List;
+import rx.Observable;
+
+public interface NetworkDataSource {
+
+  Observable<List<ImageEntity>> searchFlickerImages(String query);
+}
