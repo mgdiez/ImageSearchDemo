faf7042ab4469e560478cec4d2fef54b53a09113
Marc González Díez <marcgdiez@gmail.com>
2017-12-17 00:58:35 +0100

    Inject SearchImagesUseCase, expose Context

--- app/src/main/java/com/marcgdiez/imagesearchdemo/app/gallery/SearchGalleryPresenter.java
+++ app/src/main/java/com/marcgdiez/imagesearchdemo/app/gallery/SearchGalleryPresenter.java
@@ -1,11 +1,17 @@
 package com.marcgdiez.imagesearchdemo.app.gallery;
 
+import com.marcgdiez.imagesearchdemo.app.gallery.usecase.SearchImagesUseCase;
+import com.marcgdiez.imagesearchdemo.core.interactor.Interactor;
 import com.marcgdiez.imagesearchdemo.core.presenter.Presenter;
+import com.marcgdiez.imagesearchdemo.entity.ImageEntity;
+import java.util.List;
 import javax.inject.Inject;
 
 public class SearchGalleryPresenter extends Presenter<SearchGalleryView> {
+  private SearchImagesUseCase searchImagesUseCase;
 
-  @Inject public SearchGalleryPresenter() {
+  @Inject public SearchGalleryPresenter(Interactor<List<ImageEntity>> searchImagesUseCase) {
+    this.searchImagesUseCase = (SearchImagesUseCase) searchImagesUseCase;
   }
 
   @Override protected void initialize() {
@@ -13,5 +19,6 @@ public class SearchGalleryPresenter extends Presenter<SearchGalleryView> {
   }
 
   @Override public void stop() {
+    searchImagesUseCase.unsubscribe();
   }
 }
