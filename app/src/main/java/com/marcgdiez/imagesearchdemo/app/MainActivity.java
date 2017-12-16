e5695a951bae84521c6e02594b3414d5d94c9ddc
Marc González Díez <marcgdiez@gmail.com>
2017-12-16 21:11:13 +0100

    MainActivity now extends RootActivity and creates a SearchFragment

--- app/src/main/java/com/marcgdiez/imagesearchdemo/app/MainActivity.java
+++ app/src/main/java/com/marcgdiez/imagesearchdemo/app/MainActivity.java
@@ -1,13 +1,47 @@
 package com.marcgdiez.imagesearchdemo.app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import com.marcgdiez.imagesearchdemo.R;
+import com.marcgdiez.imagesearchdemo.app.di.module.ImageSearchComponent;
+import com.marcgdiez.imagesearchdemo.app.di.module.ImageSearchModule;
+import com.marcgdiez.imagesearchdemo.app.gallery.SearchGalleryFragment;
+import com.marcgdiez.imagesearchdemo.core.ImageSearchApplication;
+import com.marcgdiez.imagesearchdemo.core.di.HasComponent;
+import com.marcgdiez.imagesearchdemo.core.view.RootActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends RootActivity implements HasComponent<ImageSearchComponent> {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+  private ImageSearchComponent imageSearchComponent;
+
+  @Override protected void findViews() {
+
+  }
+
+  @Override protected int getLayoutResourceId() {
+    return R.layout.activity_main;
+  }
+
+  @Override protected void initializeInjector() {
+    imageSearchComponent = ((ImageSearchApplication) getApplication()).getComponent()
+        .imageSearchComponent(new ImageSearchModule());
+    imageSearchComponent.inject(this);
+  }
+
+  @Override protected void initializeActivity(Bundle savedInstanceState) {
+    addFragment(R.id.fragment_container, SearchGalleryFragment.newInstance());
+  }
+
+  protected void addFragment(@IdRes int containerId, @NonNull Fragment fragment) {
+    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+    fragmentTransaction.add(containerId, fragment);
+    fragmentTransaction.commit();
+  }
+
+  @Override public ImageSearchComponent getComponent() {
+    return imageSearchComponent;
   }
 }
