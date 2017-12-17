fb39da3994dca8e7a3d85a5568ce0ad6babad843
Marc González Díez <marcgdiez@gmail.com>
2017-12-17 01:01:46 +0100

    Create search layout, image adapter and container

--- app/src/main/java/com/marcgdiez/imagesearchdemo/app/gallery/SearchGalleryFragment.java
+++ app/src/main/java/com/marcgdiez/imagesearchdemo/app/gallery/SearchGalleryFragment.java
@@ -1,17 +1,28 @@
 package com.marcgdiez.imagesearchdemo.app.gallery;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.ContentLoadingProgressBar;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
+import butterknife.BindView;
 import com.marcgdiez.imagesearchdemo.R;
 import com.marcgdiez.imagesearchdemo.app.di.module.ImageSearchComponent;
+import com.marcgdiez.imagesearchdemo.app.gallery.adapter.ImageAdapter;
 import com.marcgdiez.imagesearchdemo.app.gallery.di.SearchGalleryModule;
 import com.marcgdiez.imagesearchdemo.core.presenter.Presenter;
 import com.marcgdiez.imagesearchdemo.core.view.RootFragment;
+import com.marcgdiez.imagesearchdemo.entity.ImageEntity;
+import java.util.List;
 import javax.inject.Inject;
 
 public class SearchGalleryFragment extends RootFragment implements SearchGalleryView {
 
   @Inject SearchGalleryPresenter presenter;
+  @Inject ImageAdapter adapter;
+
+  @BindView(R.id.recyclerView) RecyclerView recyclerView;
+  @BindView(R.id.progressBar) ContentLoadingProgressBar progressBar;
 
   public static Fragment newInstance() {
     return new SearchGalleryFragment();
@@ -26,7 +37,10 @@ public class SearchGalleryFragment extends RootFragment implements SearchGallery
   }
 
   @Override protected void initializeView(View view) {
-
+    recyclerView.setLayoutManager(new StaggeredGridLayoutManager(ImageAdapter.NUMBER_OF_COLUMNS,
+        StaggeredGridLayoutManager.VERTICAL));
+    recyclerView.setHasFixedSize(true);
+    recyclerView.setAdapter(adapter);
   }
 
   @Override protected void initializeInjector() {
@@ -37,4 +51,5 @@ public class SearchGalleryFragment extends RootFragment implements SearchGallery
   @Override protected Presenter getPresenter() {
     return presenter;
   }
+
 }
